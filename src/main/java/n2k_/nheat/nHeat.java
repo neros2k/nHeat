a17d32bfa1dcf4abe9b43becfd765f0ed706c4d0
neros2k <neros2k@gmail.com>
2022-05-27 22:40:54 +0300

    Инициализация проекта.

---
+++ src/main/java/n2k_/nheat/nHeat.java
@@ -0,0 +1,8 @@
+package n2k_.nheat;
+import org.bukkit.plugin.java.JavaPlugin;
+public final class nHeat extends JavaPlugin {
+    @Override
+    public void onEnable() {}
+    @Override
+    public void onDisable() {}
+}
